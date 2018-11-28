b19617103492e6afcc3c718b74d923c8f25b3c33
Swina Allen <swina.allen@gmail.com>
2018-11-29 00:37:42 +0300

    set correct filter_input

--- include/moodgiver.class.hide.wc.categories.php
+++ include/moodgiver.class.hide.wc.categories.php
@@ -102,7 +102,7 @@ if( !class_exists('mood_hide_categories_woocommerce') ):
     }
 
     function mg_hide_category_ajax_hide_category_woocommerce(){
-      update_term_meta(filter_input(INPUT_POST,$_POST['id']), 'wh_meta_hide', filter_input(INPUT_POST,$_POST['checked']));
+      update_term_meta(filter_input(INPUT_POST,$_POST['id'],FILTER_VALIDATE_INT), 'wh_meta_hide', filter_input(INPUT_POST,$_POST['checked'],FILTER_VALIDATE_BOOLEAN));
     }
 
     /**
